a5e16617392f41ecce7db27f423de846c686c488
Nate Geslin <teamtomkins23@gamil.com>
2016-07-22 14:25:25 -0500

    Updates install-postgresql script to supress console output during apt-get install

--- provision/shell/install-postgresql.sh
+++ provision/shell/install-postgresql.sh
@@ -6,8 +6,7 @@ echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' | sudo tee
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add - &>/dev/null
 
 sudo apt-get update &>/dev/null
-sudo apt-get install -y postgresql postgresql-client postgresql-contrib libpq-dev 
-
+sudo apt-get install -y postgresql postgresql-client postgresql-contrib libpq-dev &>/dev/null
 sudo -u postgres psql -c "create user vagrant with password 'vagrant';" &>/dev/null
 sudo -u postgres psql -c "alter user vagrant with superuser;" &>/dev/null
 
